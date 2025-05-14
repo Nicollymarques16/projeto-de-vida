e655b9d7ddfe1c1e1f4163a400ecc701dfa68813
Nicollymarques16 <159157738+Nicollymarques16@users.noreply.github.com>
2025-05-14 12:29:13 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+coonst textos = document.querySelectorAll(".aba-conteudo")
 
 for(let i =0; i <botoes.length;i++){
     botoes[i].onclik = function(){
@@ -8,5 +9,6 @@ for(let j =0;j<botoes.length; j++){
         }
 
     botoes[i].classList.add("ativo")
+        textos[j].classList.remove("ativo")
 }
 }
